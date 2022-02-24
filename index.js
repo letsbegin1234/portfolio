df607cac18558ca0d5db050329c11ac53c2f0e32
letsbegin1234 <98774745+letsbegin1234@users.noreply.github.com>
2022-02-24 22:50:45 +0530

    Add initial portfolio website files

---
+++ index.js
@@ -0,0 +1,20 @@
+AOS.init({
+    offset:400,
+    duration:2000
+});
+
+
+
+function colorChange(x) {
+    document.body.style.backgroundColor = x;
+    document.body.style.transitionDuration = "3s";
+    
+
+}
+function bgcolor() {
+    document.body.style.backgroundColor = "#D7E9F7";
+    document.body.style.transitionDuration = "3s";
+
+}
+
+
